c7e953273620abce639e6dfa968c97abc63811ba
Pavel-zhan <1786447581@qq.com>
2019-01-09 01:32:03 -0800

    Third Version

---
+++ 20190108Pavel/Singleton2.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    Singleton2.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2019-01-09 00:10:04
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+ 
+int main(void)
+{
+ 
+}
